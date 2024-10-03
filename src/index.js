b25817a17e57cc1815e19dcf13ccead493d6a4d8
Hosea <karanja.mungai23@gmail.com>
2024-10-03 08:58:11 +0300

    modifies index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+    const todoContainer = document.querySelector('#list');
+    const createTodo = document.createElement('li');
+    const myToDo = document.querySelector('#new-task-description');
+    const timeDue = document.getElementById('time')  
+    console.log(timeDue.value) 
+    createTodo.innerText =` ${myToDo.value} (due at ${timeDue.value})`
+    todoContainer.appendChild(createTodo);
+    const deleteBtn = document.createElement('button');
+    createTodo.appendChild(deleteBtn)
+    deleteBtn.textContent = 'X';
+    deleteBtn.style.fontsize = "large"
+    deleteBtn.style.marginLeft = "5px"
+    deleteBtn.addEventListener('click', function (event){
+      event.target.parentNode.remove();
+    })
+
+
+    
+  form.reset()
+  })
 });
